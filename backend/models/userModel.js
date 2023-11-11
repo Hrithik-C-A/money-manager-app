8da778007fe6c257ef021dfe6a63f6ac91740141
Hrithik C A <hrithikca@gmail.com>
2023-11-11 17:44:46 +0530

    password hasing working perfectly

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const { Schema, model } = mongoose;
 
@@ -20,6 +21,20 @@ const userSchema = new Schema({
         type: Boolean,
         default: false
     }
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password =  await bcrypt.hash('', salt);
 });
 
 const User = model('User',userSchema);
