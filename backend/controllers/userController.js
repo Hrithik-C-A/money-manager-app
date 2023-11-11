83740d1f0985bb5d95821295c249ef617109e835
Hrithik C A <hrithikca@gmail.com>
2023-11-11 13:12:21 +0530

    User registering endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,8 +4,29 @@ const login = (req, res) => {
     res.send('Im login endpoint');
 };
 
-const register = (req, res) => {
-    res.send('Im register endpoint');
+const register = async (req, res) => {
+    const { name, email, password, isAdmin } = req.body;
+
+    try {
+        const user = await User.create({name, email, password, isAdmin});
+        const createdUser = await user.save();
+        res.status(200).json({
+            message: 'User registered successfully',
+            user: {
+                name: createdUser.name,
+                email: createdUser.email
+            },
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: 'User not registered',
+        })
+    }
+    
+
+    
+    
+    
 };
 
 export { login, register };
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 connectDB();
 
