57e67720f6901ab9fd5392940b1e84c6536c2ef7
Hrithik C A <hrithikca@gmail.com>
2023-11-11 13:34:45 +0530

    user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,26 +7,30 @@ const login = (req, res) => {
 const register = async (req, res) => {
     const { name, email, password, isAdmin } = req.body;
 
-    try {
-        const user = await User.create({name, email, password, isAdmin});
-        const createdUser = await user.save();
-        res.status(200).json({
-            message: 'User registered successfully',
-            user: {
-                name: createdUser.name,
-                email: createdUser.email
-            },
-        });
-    } catch (error) {
-        res.status(400).json({
-            message: 'User not registered',
-        })
-    }
-    
+    const userExists = await User.findOne({ email });
 
-    
-    
-    
+    if (userExists) {
+        res.status(409).json({
+            message: 'Account already exists. Either login or register with a new email.'
+        })
+    } else {
+        try {
+            const user = await User.create({name, email, password, isAdmin});
+            const createdUser = await user.save();
+            res.status(200).json({
+                message: 'User registered successfully',
+                user: {
+                    name: createdUser.name,
+                    email: createdUser.email
+                },
+            });
+        } catch (error) {
+            res.status(400).json({
+                message: 'User not registered',
+            })
+        }
+    }
+        
 };
 
 export { login, register };
