d73bc0c34847ee35654c905b9bb9d8aeb33196c8
Hrithik C A <hrithikca@gmail.com>
2023-11-12 10:30:52 +0530

    added cookie-parser middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 connectDB();
 
