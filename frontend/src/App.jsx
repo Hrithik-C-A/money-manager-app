13665d9cebc341e4000ac957865bfb8760696803
Hrithik C A <hrithikca@gmail.com>
2023-11-23 11:02:09 +0530

    router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <Outlet/>
+      </div>
+    </div>
   )
 }
 
